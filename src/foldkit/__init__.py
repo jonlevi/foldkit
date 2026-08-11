e3027e0cc32167fbcdfaea160cf112d9acf15d21
jonlevi <jonlevi@users.noreply.github.com>
2026-08-11 21:23:47 +0000

    deploy: cfa98ff21e3eefd3fe6958e3de9a5172c3df1773

--- src/foldkit/__init__.py
+++
@@ -1,6 +0,0 @@
-from .af3_result import AF3Result
-from .af3_ensemble import AF3Ensemble
-from .storage import save_af3_result
-
-__all__ = ["AF3Result", "AF3Ensemble", "save_af3_result"]
-__version__ = "0.1.3"
